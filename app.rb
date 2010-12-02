1db171cf6d28454d8f33973c954137a8f4d62ae2
andershaig <andershaig@gmail.com>
2010-12-02 14:36:47 -0600

    Some removals for testing

--- app.rb
+++ app.rb
@@ -1,26 +1,10 @@
 require 'sinatra'
 require 'quick_magick'
 
-configure do
-  environment = Sinatra::Application.environment.to_s
-end
-
-class Accio
-  def self.english_to_pig_latin(text)
-    text.split.map do |word|
-      if word.length <= 2
-        word
-      else
-        (word[1,9999] + word[0,1] + "ay").downcase
-      end
-    end.join(" ")
-  end
-end
-
 get '/' do
-  i = QuickMagick::Image.read('oldmona.jpg').first
-  i.resize "300x300!"
-  i.save "newmona.jpg"
+#  i = QuickMagick::Image.read('oldmona.jpg').first
+#  i.resize "300x300!"
+#  i.save "newmona.jpg"
   erb :output
 end
 
